e766596c13119f1ca4015aeed867ca4a76478374
Gustavo <guvono4@gmail.com>
2024-04-22 23:08:01 -0300

    Add project files.

---
+++ fibonacci_sequence/Program.cs
@@ -0,0 +1,32 @@
+﻿int restart = 0, numero = 1, ultimo = 1, penultimo = -1, atual = 0;
+
+
+do
+{
+    ultimo = 1;
+    penultimo = -1;
+    atual = 0;
+
+    do
+    {
+        Console.Clear();
+
+        if (numero <= 0) Console.WriteLine("ERRO: Informe um número maior que 0");
+
+        Console.Write("Informe quantos números da sequência fibonacci você deseja ver:");
+        numero = int.Parse(Console.ReadLine());
+    } while (numero <= 0);
+
+    for (int i = 0; i < numero; i++)
+    {
+        atual = ultimo + penultimo;
+        Console.Write(atual + "  ");
+        penultimo = ultimo;
+        ultimo = atual;
+    }
+
+    Console.WriteLine("\nDeseja reiniciar o programa?");
+    Console.WriteLine("1 - SIM");
+    Console.WriteLine("2 - NAO");
+    restart = int.Parse(Console.ReadLine());
+} while (restart == 1);
